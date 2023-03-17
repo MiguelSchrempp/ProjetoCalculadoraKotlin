7afe36fa761945d8b3cc51acc8e7cd79e959fa83
MiguelSchrempp <miguel.schrempp@sptech.school>
2023-03-16 23:33:25 -0300

    Calculadora em spring boot com postman

---
+++ CalculadoraKotlinSpringboot/src/main/kotlin/miguel/schrempp/calculadorakotlinspringboot/Calculadora.kt
@@ -0,0 +1,12 @@
+package miguel.schrempp.calculadorakotlinspringboot
+
+class Calculadora {
+    val n1 : Double
+    val n2 : Double
+
+    constructor(n1: Double, n2: Double) {
+        this.n1 = n1
+        this.n2 = n2
+    }
+
+}
---
+++ CalculadoraKotlinSpringboot/src/test/kotlin/miguel/schrempp/calculadorakotlinspringboot/CalculadoraKotlinSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package miguel.schrempp.calculadorakotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalculadoraKotlinSpringbootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
